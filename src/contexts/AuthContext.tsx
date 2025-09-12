4fdeda0fa647308792da6112b21531c023553213
hevolife <hevolife@gmail.com>
2025-09-13 01:50:04 +0200

    Fix return outside function error

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -23,7 +23,6 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within AuthProvider');
-  }
   return context;
 };
 
