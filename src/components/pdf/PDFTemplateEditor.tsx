9042be38b3d8446c316d2f61a68e97c6c3edad03
hevolife <hevolife@gmail.com>
2025-09-12 16:39:21 +0200

    Fix useRef import error

--- src/components/pdf/PDFTemplateEditor.tsx
+++ src/components/pdf/PDFTemplateEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { PDFViewer } from './PDFViewer';
