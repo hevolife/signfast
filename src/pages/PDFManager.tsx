01fd18492f0ce055fa12a28af0091d4a0183b52a
hevolife <hevolife@gmail.com>
2025-09-13 01:20:44 +0200

    Fix supabase import error in PDFManager

--- src/pages/PDFManager.tsx
+++ src/pages/PDFManager.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { supabase } from '../lib/supabase';
 import { PDFService } from '../services/pdfService';
 import { formatDateTimeFR } from '../utils/dateFormatter';
 import { useLimits } from '../hooks/useLimits';
