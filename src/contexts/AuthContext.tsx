2971d019f23eb962e562ea275276c27b32f02427
hevolife <hevolife@gmail.com>
2025-09-13 01:50:06 +0200

    Updated AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -23,6 +23,7 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within AuthProvider');
+  }
   return context;
 };
 
