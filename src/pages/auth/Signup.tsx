bcea8f410220d3375bb6e8a5656a47ed79585daa
hevolife <hevolife@gmail.com>
2025-09-13 18:25:59 +0200

    Remove presentation link from signup page

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -122,16 +122,6 @@ export const Signup: React.FC = () => {
                   Se connecter
                 </Link>
               </p>
-              <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">
-                Ou{' '}
-                <Link
-                  to="/"
-                  state={{ from: { pathname: '/signup' } }}
-                  className="font-medium text-blue-600 hover:text-blue-500"
-                >
-                  voir la présentation
-                </Link>
-              </p>
             </div>
           </CardContent>
         </Card>
