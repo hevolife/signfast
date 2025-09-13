aa2b05cf4212c527e1bbcfcafcc3c7b49ec0586d
hevolife <hevolife@gmail.com>
2025-09-13 23:46:43 +0200

    Updated LimitReachedModal.tsx

--- src/components/subscription/LimitReachedModal.tsx
+++ src/components/subscription/LimitReachedModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { supabase } from '../../lib/supabase';
 import { Link } from 'react-router-dom';
 import { Button } from '../ui/Button';
 import { Card, CardContent, CardHeader } from '../ui/Card';
