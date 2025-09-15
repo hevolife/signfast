c8de64c4d5099ddc735712b80ec0e84997d3aeee
hevolife <hevolife@gmail.com>
2025-09-15 19:20:44 +0200

    Updated PDFManager.tsx

--- src/pages/PDFManager.tsx
+++ src/pages/PDFManager.tsx
@@ -892,10 +892,6 @@ export const PDFManager: React.FC = () => {
               </Card>
             ))}
           </div>
-                </div>
-              )}
-            </CardContent>
-          </Card>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
             {filteredAndSortedResponses.map((response, index) => {
